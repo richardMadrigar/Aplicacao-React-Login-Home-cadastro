9d071352af21fa870ceac3411105c32e4a7349e2
Richard madrigar <richard_madrigar99@hotmail.com>
2021-09-30 14:50:39 -0300

    teste3

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 //imports components
 import Logo from '../../components/Logo/Logo'
-import Button from '../../components/Button/Button'
+import Button from '../../components/Button'
 
 
 const Home = (props) => {
